019c0b3eaa4d5fd8bb927d9fa2f11f594a1cbbfa
uzquiano <mgu1@cornell.edu>
2014-01-19 18:00:46 -0500

    Added Alpaca forms examples for 5 forms Added dependency libraries straight away for the moment (will include in build later) Missing schemas -   complaints.json/#complaint   symptoms/#symptoms   diseases/#disease   forms/#formlist

---
+++ forms/ltfhc.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+    var connector = Alpaca.Connector.extend({
+
+        loadData: function(resource, successCallback, errorCallback)
+        {
+            var data = {};
+
+            successCallback(data);
+        },
+
+        loadSchema: function(resource, successCallback, errorCallback)
+        {
+            if (typeof(resource) == "string")
+            {
+                resource = "/schema/" + resource + ".json"
+            }
+
+            return this.base(resource, successCallback, errorCallback);
+        },
+
+        loadOptions: function(resource, successCallback, errorCallback)
+        {
+            if (typeof(resource) == "string")
+            {
+                resource = "/forms/options/" + resource + ".json"
+            }
+
+            return this.base(resource, successCallback, errorCallback);
+        }
+    });
+
+    Alpaca.registerConnectorClass("default", connector);
+
+    var renderForm = function(type)
+    {
+        $("#form").empty();
+
+        if (type)
+        {
+            $("#form").alpaca({
+                "schemaSource": type,
+                "optionsSource": type,
+                "dataSource": type,
+                "postRender": postRenderCallback,
+                "ui": "bootstrap",
+                "type": "create"
+            });
+        }
+    };
+
+    var postRenderCallback = function(control)
+    {
+        // manually handle the submit
+        control.form.getEl().submit(function(e) {
+            e.preventDefault();
+            alert(JSON.stringify(control.getValue(), null, "    "));
+        });
+    };
+
+    // handle form type links
+    $(".formLink").click(function(e) {
+        e.preventDefault();
+
+        var dataFormType = $(this).attr("data-form-type");
+        renderForm(dataFormType);
+    });
+});
