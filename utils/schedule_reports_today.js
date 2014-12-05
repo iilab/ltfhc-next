b96457bbc6cf17bd6f43c30b0da8a723b4ef9535
Caolan McMahon <caolan@caolanmcmahon.com>
2014-12-05 15:25:42 +0000

    fix schedule reports for today dev util script

--- utils/schedule_reports_today.js
+++ utils/schedule_reports_today.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-console.log(
-    "WARNING: this script doesn't currenty work! Perhaps I'm misunderstanding" +
-    "the schedule document structure..."
-);
-
 var async = require('async');
 var couchr = require('couchr');
 var moment = require('moment');
@@ -21,24 +16,28 @@ var reports = require('../views/lib/reports_schema').reports();
 var report_names = Object.keys(reports);
 
 async.forEachSeries(report_names, function (report, cb) {
-    // no _rev so we only create doc if one doesn't already exist
-    var schedule_doc = {
-        _id: 'schedule_' + report,
-        class: 'entry',
-        collection: 'schedule',
-        doc_collection: 'reports',
-        date_scheduled: moment().toString()
-    };
-    couchr.post(dburl, schedule_doc, function (err) {
-        if (err) {
-            // log errors and continue processing
-            console.error('Error creating schedule doc for: ' + report);
-            console.error(err);
+    var docurl = dburl + '/schedule_' + report;
+    couchr.get(docurl, {}, function (err, doc) {
+        if (err && err.error === 'not_found') {
+            doc = {
+                _id: 'schedule_' + report,
+                class: 'entry',
+                collection: 'schedule',
+                doc_collection: 'reports'
+            };
         }
-        else {
-            console.log('Created schedule doc for: ' + report);
-        }
-        return cb();
+        doc.date_scheduled = moment().toISOString();
+        couchr.put(docurl, doc, function (err) {
+            if (err) {
+                // log errors and continue processing
+                console.error('Error updating schedule doc for: ' + report);
+                console.error(err.error + ': ' + err.reason);
+            }
+            else {
+                console.log('Updated schedule doc for: ' + report);
+            }
+            return cb();
+        });
     });
 },
 function (err) {
